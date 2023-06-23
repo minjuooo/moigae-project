7c17f1b52c9fd7f475b9e7530cf8b30ffe7d1e87
YangJungah <130117724+YangJungah@users.noreply.github.com>
2023-06-23 17:50:47 +0900

    금욜 풀리퀘 (#65)
    * feat: 화면 최종 검수 step1 (#1)
    
    * feat: 화면 최종 검수 step1 (#2)
    
    * 중간 저장 (#3)

--- src/main/java/com/moigae/application/component/MypageController.java
+++ src/main/java/com/moigae/application/component/MypageController.java
@@ -48,30 +48,5 @@ public class MypageController {
         model.addAttribute("customUser", customUser);
         return "users/mypageUnJoin";
     }
-    @GetMapping("/mypageFix")
-    public String myPageFix(Model model, @AuthenticationPrincipal CustomUser customUser) {
-        User user = userRepository.findById(customUser.getId())
-                .orElseThrow(() -> new ResourceNotFoundException("User not found with id"));
-
-        model.addAttribute("customUser", customUser);
-        model.addAttribute("user", user);
-        return "users/mypageFix";
-    }
-//    @PostMapping("/users/update")
-//    public String updateUserProfile(@RequestParam("id") String id,
-//                                    @RequestParam("name") String name,
-//                                    @RequestParam("email") String email,
-//                                    @RequestParam("password") String password) {
-//        User user = userRepository.findById(id)
-//                .orElseThrow(() -> new ResourceNotFoundException("User not found with id"));
-//
-//        user.setId(name);
-//        user.setEmail(email);
-//        user.setPassword(password);
-//
-//        userRepository.save(user);
-//
-//        return "redirect:/users/mypage";
-//    }
 
 }
