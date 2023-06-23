a62778557f196d119c8c702cd05a6e05ddd3d474
Choi WukJae <57352043+chldnrwo@users.noreply.github.com>
2023-06-23 16:04:41 +0900

    기존 업무 종료 (#62)
    * JPA에 다는 CASCADE는 DB의 외래키 관계까지 CASCADE로 설정해주지 않음 (#26)
    
    따라서 JPA에서 추가한 CASCADE로 keystoneJS 삭제가 원활해지진 않는다. CASCADE는 개발끝나고 DB단위로 달것

--- src/main/java/com/moigae/application/component/user/application/UserService.java
+++ src/main/java/com/moigae/application/component/user/application/UserService.java
@@ -2,15 +2,20 @@ package com.moigae.application.component.user.application;
 
 import com.moigae.application.component.user.api.request.UserLoginForm;
 import com.moigae.application.component.user.domain.User;
+import com.moigae.application.component.user.domain.enumeration.UserRole;
 import com.moigae.application.component.user.dto.CustomUser;
 import com.moigae.application.component.user.dto.UserDto;
 import com.moigae.application.component.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,11 +36,17 @@ public class UserService implements UserDetailsService {
         if (user == null) {
             throw new IllegalArgumentException("존재하지 않는 email 입니다.");
         }
+        boolean admin = false;
+        if (user.getUserRole() != null && user.getUserRole().equals(UserRole.ADMIN)) {
+            admin = true;
+        }
+
         CustomUser customUser = new CustomUser();
         customUser.setUsername(user.getEmail());
         customUser.setPassword(user.getPassword());
         customUser.setId(user.getId());
         customUser.setName(user.getUserName());
+        customUser.setAdmin(admin);
 
         // customUser 객체 설정...
         return customUser;
--- src/main/java/com/moigae/application/core/config/SecurityConfig.java
+++ src/main/java/com/moigae/application/core/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.moigae.application.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.web.firewall.HttpFirewall;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
